5ef0c1381f81fc58cff0f61cbabfa863ee7f5ee7
vphatfla <pvphat25@gmail.com>
2023-04-01 17:57:49 -0400

    clean class files

--- week2/LineSegment.java
+++ week2/LineSegment.java
@@ -1,3 +1,5 @@
+package week2;
+
 /*************************************************************************
  *  Compilation:  javac LineSegment.java
  *  Execution:    none
--- week2/Main.java
+++ week2/Main.java
@@ -1,3 +1,5 @@
+package week2;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
